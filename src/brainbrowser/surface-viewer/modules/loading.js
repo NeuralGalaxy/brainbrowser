7807e5d114e835492419566f9a0d9a20ceb3295b
jiangheQu <jianghe.qu@neuralgalaxy.com>
2021-01-15 15:18:55 +0800

    #5856 Add format data cb

--- src/brainbrowser/surface-viewer/modules/loading.js
+++ src/brainbrowser/surface-viewer/modules/loading.js
@@ -275,8 +275,9 @@ BrainBrowser.SurfaceViewer.modules.loading = function(viewer) {
     }, { ...options, isSurface: true });
   };
 
-  viewer.loadIntensityDataFromText = function(text, options) {
+  viewer.loadIntensityDataFromText = function(text, options, formatCb) {
     options = checkBinary("intensity_data_types", options);
+    text = formatCb ? formatCb(text) : text;
     loadIntensityData(text, undefined, options);
   };
 
